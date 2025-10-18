7b0a4c354241021fb2b60e4d16d7d31b04020c56
limnicholased <nicholaskaijielim@gmail.com>
2025-10-18 20:09:55 +1100

    chore(infra): basic scaffolding

---
+++ dbt/macros/util.sql
@@ -0,0 +1,3 @@
+﻿{% macro hz() %} {{ var('hz') }} {% endmacro %}
+{% macro sprint_speed_mps() %} {{ var('sprint_speed_mps') }} {% endmacro %}
+{% macro min_duration_s() %} {{ var('min_duration_s') }} {% endmacro %}
